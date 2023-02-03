3054f8a549304a763d425b010ea0aa7e4b706af9
quang-ngh <quangngcs@gmail.com>
2023-02-03 08:59:18 +0000

    update file main

--- diffusion/main.py
+++ diffusion/main.py
@@ -1,2 +1,13 @@
 import torch
 from diffusers import StableDiffusionPipeline
+
+if __name__ == '__main__':
+
+    model_id = "CompVis/stable-diffusion-v1-4"
+    device = "cuda"
+
+
+    pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
+    pipe = pipe.to(device)  
+
+    
