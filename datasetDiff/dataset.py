1d75ebd2ad6756cdcc418e55b7213d646bf02089
quang-ngh <quangngcs@gmail.com>
2023-02-23 09:54:00 +0700

    update gitignore

--- datasetDiff/dataset.py
+++ datasetDiff/dataset.py
@@ -1,15 +1,16 @@
 import torch
+import torchvision
 from torch.utils.data import Dataset, DataLoader
 
 class AbstractDataset(Dataset):
-    def __init__(self, root: str, split: int, base_size, augment: bool, val: bool, scale: bool, flip: bool, *args, **kwargs) -> None:
+    def __init__(self, root: str, split: int, base_size, augment: bool, val: bool, scale: bool, *args, **kwargs) -> None:
         """
         --------------------------------
         Parameters:
             root: 
                 root path of the dataset
             split: 
-                ratio of split
+                ratio of split 
             base_size: 
                 expected size of the dataset
             augment: 
@@ -20,17 +21,30 @@ class AbstractDataset(Dataset):
                 scale up or down the original image
             flip: 
                 random flip the image
-
-        """
+            augmentor: 
+                combination of random transformations to augment the data
+    """
         self.root = root
         self.splot = split
         self.base_size = base_size
         self.augment = augment
         self.val = val
         self.scale = scale
-        self.flip  = flip
-
+        self.toTensor = torchvision.transforms.ToTensor()  # Automatic scale images to [0,1]
+        self.augmentor = None
+        
+        if self.augment:
+            augment_composer = [
+                torchvision.transforms.CenterCrop(self.base_size),
+                torchvision.transforms.RandomHorizontalFlip(p = 0.5),
+            ]
+            self.augmentor = torchvision.transforms.Compose([
+                *augment_composer
+            ])
         super(AbstractDataset, self).__init__(**kwargs)
+    
+    def _set_files(self):
+        raise NotImplementedError
     
     def _load_data(self):
         raise NotImplementedError
@@ -50,9 +64,23 @@ class ImageCaptioningDataset(AbstractDataset):
                     dataset = ImageCaptioningDataset(root = ".", modal = "image")       # dataset just contains images
         """         
         super(ImageCaptioningDataset, self).__init__()
+        self.modal = modal
         
-        pass
-
+        def _load_data(self, index: int):
+            
+            if not self.modal in ['image', 'text', 'both']:
+                raise ValueError("Dataset must includes at least image or text! Domain is not compatible")
+            
+            if self.modal == "text":               # Just get the captions
+                pass
+            elif self.modal == "image":            # Just get the images
+                pass
+            else:                                  # Default: pairs of (image, text)
+                pass
+            
+            pass
+        
+        def 
 class SegmentationDataset(AbstractDataset):
     def __init__(self) -> None:
         super(SegmentationDataset, self).__init__()
